8ef1ae9800b965b1373225708afebfab4193f237
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-12-15 12:15:13 -0600

    search bar

--- src/App.vue
+++ src/App.vue
@@ -14,27 +14,27 @@ let map = reactive(
         },
         zoom: 12,
         bounds: {
-            nw: {lat: 45.008206, lng: -93.217977},
-            se: {lat: 44.883658, lng: -92.993787}
+            nw: { lat: 45.008206, lng: -93.217977 },
+            se: { lat: 44.883658, lng: -92.993787 }
         },
         neighborhood_markers: [
-            {location: [44.942068, -93.020521], marker: null},
-            {location: [44.977413, -93.025156], marker: null},
-            {location: [44.931244, -93.079578], marker: null},
-            {location: [44.956192, -93.060189], marker: null},
-            {location: [44.978883, -93.068163], marker: null},
-            {location: [44.975766, -93.113887], marker: null},
-            {location: [44.959639, -93.121271], marker: null},
-            {location: [44.947700, -93.128505], marker: null},
-            {location: [44.930276, -93.119911], marker: null},
-            {location: [44.982752, -93.147910], marker: null},
-            {location: [44.963631, -93.167548], marker: null},
-            {location: [44.973971, -93.197965], marker: null},
-            {location: [44.949043, -93.178261], marker: null},
-            {location: [44.934848, -93.176736], marker: null},
-            {location: [44.913106, -93.170779], marker: null},
-            {location: [44.937705, -93.136997], marker: null},
-            {location: [44.949203, -93.093739], marker: null}
+            { location: [44.942068, -93.020521], marker: null },
+            { location: [44.977413, -93.025156], marker: null },
+            { location: [44.931244, -93.079578], marker: null },
+            { location: [44.956192, -93.060189], marker: null },
+            { location: [44.978883, -93.068163], marker: null },
+            { location: [44.975766, -93.113887], marker: null },
+            { location: [44.959639, -93.121271], marker: null },
+            { location: [44.947700, -93.128505], marker: null },
+            { location: [44.930276, -93.119911], marker: null },
+            { location: [44.982752, -93.147910], marker: null },
+            { location: [44.963631, -93.167548], marker: null },
+            { location: [44.973971, -93.197965], marker: null },
+            { location: [44.949043, -93.178261], marker: null },
+            { location: [44.934848, -93.176736], marker: null },
+            { location: [44.913106, -93.170779], marker: null },
+            { location: [44.937705, -93.136997], marker: null },
+            { location: [44.949203, -93.093739], marker: null }
         ]
     }
 );
@@ -54,17 +54,17 @@ onMounted(() => {
     let district_boundary = new L.geoJson();
     district_boundary.addTo(map.leaflet);
     fetch('data/StPaulDistrictCouncil.geojson')
-    .then((response) => {
-        return response.json();
-    })
-    .then((result) => {
-        result.features.forEach((value) => {
-            district_boundary.addData(value);
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            result.features.forEach((value) => {
+                district_boundary.addData(value);
+            });
+        })
+        .catch((error) => {
+            console.log('Error:', error);
         });
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-    });
 });
 
 
@@ -96,9 +96,20 @@ function closeDialog() {
         <label class="dialog-label">URL: </label>
         <input id="dialog-url" class="dialog-input" type="url" v-model="crime_url" placeholder="http://localhost:8000" />
         <p class="dialog-error" v-if="dialog_err">Error: must enter valid URL</p>
-        <br/>
+        <br />
         <button class="button" type="button" @click="closeDialog">OK</button>
     </dialog>
+    <div class="grid-container ">
+        <div class="row">
+            <div class="small-12 large-7 columns">
+                <input id="addressDialog" class="dialog-input"
+                    placeholder="2115 Summit Ave, Saint Paul, MN 55105, United States" />
+            </div>
+            <div class="small-12 large-4 columns">
+                <button class="button" type='button'>Search</button>
+            </div>
+        </div>
+    </div>
     <div class="grid-container ">
         <div class="grid-x grid-padding-x">
             <div id="leafletmap" class="cell auto"></div>
