e79cdf130b390bedd175e3ad5d96447426a9ce6e
DevDroid42 <19912037+DevDroid42@users.noreply.github.com>
2023-12-21 17:29:18 -0600

    router works

---
+++ src/aboutProject.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+  <!-- Your page content lives here -->
+  <h1 style="text-align: center">About The Project</h1>
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell small-12 medium-4">
+        <h2 style="text-align: center">Ethan</h2>
+        <img class="align-center" src="" alt="picture of Ethan Honzik" />
+        <div class="cell small-12 medium-8 large-8">
+          <p style="text-align: center">information</p>
+        </div>
+      </div>
+
+      <div class="cell small-12 medium-4">
+        <h2 style="text-align: center">Tanner</h2>
+        <img class="align-center" src="" alt="picture of Ethan Honzik" />
+        <div class="cell small-12 medium-8 large-8">
+          <p style="text-align: center">information</p>
+        </div>
+      </div>
+
+      <div class="cell small-12 medium-4">
+        <h2 style="text-align: center">Furaha</h2>
+        <img class="align-center" src="" alt="picture of Furaha Aswan" />
+        <div class="cell small-12 medium-8 large-8">
+          <p style="text-align: center"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import App from './App.vue'
+import map from './map.vue'
+import about from './aboutProject.vue'
 
-createApp(App).mount('#app');
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Vue',  component: App},
+        {path: '/map', name: 'map',  component: map},
+        {path: '/about', name:'about', component: about}
+    ]
+})
+
+createApp(App).use(router).mount('#app');
